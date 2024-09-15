6b9a8797b950e989454ed6283c118fdb4982400c
galamo <galamouyal88@gmail.com>
2024-09-15 21:08:38 +0300

    a

--- api/src/customers/index.ts
+++ api/src/customers/index.ts
@@ -1,5 +1,8 @@
 import express from "express"
 import { getCustomers } from "./handlers/getCustomers"
+import { createCustomer, CustomerType } from "./handlers/createCustomer"
+import { deleteCustomer } from "./handlers/deleteCustomer"
+import { updateCustomer } from "./handlers/updateCustomer"
 
 const router = express.Router()
 
@@ -11,7 +14,40 @@ router.get("/", async (req, res, next) => {
         res.send("Something went wrong")
     }
 })
+router.post("/", async (req, res, next) => {
+    try {
+        const newCustomer: CustomerType = extractCustomer(req.body)
+        const result = await createCustomer(newCustomer)
+        res.json({ result })
+    } catch (error) {
+        res.send("Something went wrong")
+    }
+})
+router.delete("/:idToDelete", async (req, res, next) => {
+    try {
+        const affectedRows = await deleteCustomer(+req.params.idToDelete)
+        res.json({ affectedRows })
+    } catch (error) {
+        res.send("Something went wrong")
+    }
+})
+router.put("/:idToUpdate", async (req, res, next) => {
+    try {
+        console.log("put")
+        const customerToUpdate = extractCustomer(req.body)
+        console.log(customerToUpdate, req.params.idToUpdate)
+        const affectedRows = await updateCustomer(+req.params.idToUpdate, customerToUpdate)
+        res.json({ affectedRows })
+    } catch (error) {
+        console.log(error)
+        res.send("Something went wrong")
+    }
+})
 
-// router.post("/",(CREATE A FLOW FOR NEW CUSTOMER)=>{})
+
+function extractCustomer(body: any): CustomerType {
+    const { firstName, lastName, company, email, jobTitle } = body;
+    return { firstName, lastName, company, email, jobTitle };
+}
 
 export { router }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
 import { router as customersRouter } from "./customers"
+import bodyParser from "body-parser"
 dotenv.config()
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get("/health-check", (req, res, next) => {
     return res.json({ message: "Ok" })
