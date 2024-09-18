7b3c245b70962b27ff9484422ae73845457f3481
galamo <galamouyal88@gmail.com>
2024-09-18 21:13:15 +0300

    a

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,10 @@ import express from "express"
 import dotenv from "dotenv"
 import { router as customersRouter } from "./customers"
 import bodyParser from "body-parser"
+import cors from "cors"
 dotenv.config()
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json())
 
 app.get("/health-check", (req, res, next) => {
